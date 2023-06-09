c0019905aa9dab3512e50412d282a596e479d23e
RamunnoAJ <ramunnoagustin@gmail.com>
2023-06-08 22:02:14 -0300

    agrega testeos que verifican el renderizado de pokemones

--- src/ui/__tests__/listado.spec.js
+++ src/ui/__tests__/listado.spec.js
@@ -9,7 +9,7 @@ test('actualiza el texto indice', () => {
   expect(document.querySelector('#indice').textContent).toContain('test')
 })
 
-test('muestra el listado de pokemones', () => {
+test('muestra el listado de pokemones con una función de callback', () => {
   document.body.innerHTML = '<div id="indice"></div>'
   const mockCallBack = jest.fn()
   const pokemones = [
@@ -25,4 +25,17 @@ test('muestra el listado de pokemones', () => {
   })
 
   expect(mockCallBack).toBeCalledTimes(3)
+  expect(mockCallBack).toBeCalledWith('Bulbasaur')
+  expect(mockCallBack).toBeCalledWith('Ivysaur')
+  expect(mockCallBack).toBeCalledWith('Venusaur')
+})
+
+test('muestra el listado de pokemones sin una función de callback', () => {
+  document.body.innerHTML = '<div id="indice"></div>'
+  const pokemones = [
+    { name: 'Bulbasaur' },
+    { name: 'Ivysaur' },
+    { name: 'Venusaur' }
+  ]
+  mostrarListadoPokemones(pokemones)
 })
